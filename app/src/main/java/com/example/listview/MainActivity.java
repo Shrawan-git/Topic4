0d8b3891f3dc2cc85a60c2c208409ce1ff12bfc1
shrawan <shrawanshaky3@gmail.com>
2019-10-25 10:23:18 +0545

    my commit

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView country;
+
+    public static final String countries[]={"Nepal","China","India"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        country = findViewById(R.id.list);
+
+        ArrayAdapter adapter = new ArrayAdapter()
     }
 }
